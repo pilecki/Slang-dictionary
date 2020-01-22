ef4fa7d86f1880814951596fb80dfb86031226a0
Maciej Laskowski <maciej.laskowski@yahoo.com>
2020-01-22 03:14:03 +0000

    A base.html file in the templates with general layout for the website was made and application was correctly  connected to Mongo database

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'dictionary'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return '<h1>Hello World1</h1>'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("words.html", words=mongo.db.words.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+            
+print("MONGO_URI")
